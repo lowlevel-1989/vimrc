0069e1b662ef411b1d367fb41e658efd755ad9de
formatcom <formatcom@localhost.localdomain>
2016-02-09 21:26:41 -0500

    se agrego soporte a fedora

---
+++ README.md
@@ -0,0 +1,14 @@
+###########################################################################
+###                              ...AUTOR...                            ###
+###                            Vinicio Valbuena                         ###
+###########################################################################
+###                          ...INFORMACION...                          ###
+### SABER NUMEROS DE NUCLEOS -> grep processor /proc/cpuinfo | wc -l    ###
+###########################################################################
+###                          ...INSTALACION...                          ###     
+### UBUNTU -> ./install.sh                                              ###
+###           sudo ./install.sh youCompleteMe 2 ubuntu                  ###
+### FEDORA -> ./install.sh                                              ###
+###           sudo ./install.sh youCompleteMe 2 fedora                  ###
+###########################################################################
+
--- install.sh
+++ install.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
-DISTRO=$1
-CORES=1
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~
-vim +PluginInstall +qall
+###########################################################################
+###                              ...AUTOR...                            ###
+###                           Vinicio Valbuena                          ###
+###########################################################################
+###                           ...INFORMACION...                         ###
+### SABER NUMEROS DE NUCLEOS -> grep processor /proc/cpuinfo | wc -l    ###
+###########################################################################
+###                           ...INSTALACION...                         ###                     
+### UBUNTU -> ./install.sh ~ plus                                       ###
+###           sudo ./install.sh ~ youCompleteMe 2 ubuntu                ###
+### FEDORA -> ./install.sh ~ plus                                       ###
+###           sudo ./install.sh ~ youCompleteMe 2 fedora                ###
+###########################################################################
 
-if [ "$DISTRO" = 'fedora' ]; then
-  dnf install -fs automake gcc gcc-c++ kernel-devel cmake
-  dnf install -fs python-devel
-else
-  apt-get install -fs build-essential 
-  apt-get install -fs python-dev
-  
+HOME=$1
+YOUCOMPLETEME=$2
+CORES=$3
+DISTRO=$4
+
+if [ "$YOUCOMPLETEME" = 'youCompleteMe' ]; then
+
+  if [ "$DISTRO" = 'fedora' ]; then
+    dnf install -y automake gcc gcc-c++ kernel-devel
+    dnf install -y python-devel
+  fi
+
+  if [ "$DISTRO" = 'ubuntu' ]; then
+    apt-get install -fs build-essential 
+    apt-get install -fs python-dev
+  fi
+
+  git clone https://github.com/Valloric/YouCompleteMe.git $HOME/.vim/bundle/YouCompleteMe
+
   wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz
   tar -xzvf cmake-3.4.3.tar.gz
   rm -f cmake-3.4.3.tar.gz
@@ -22,7 +42,19 @@ else
   make install
   cd ..
   rm -rf cmake-3.4.3
-fi
 
-cd ~/.vim/bundle/YouCompleteMe
-YCM_CORES=$CORES python install.py --clang-completer --tern-completer
+  cd $HOME/.vim/bundle/YouCompleteMe
+  YCM_CORES=$CORES python install.py --clang-completer --tern-completer
+else
+
+  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+  if [ "$YOUCOMPLETEME" = 'plus' ]; then
+    cp vimrcx $HOME/.vimrc
+  else
+    cp vimrc $HOME/.vimrc
+  fi
+
+  vim +PluginInstall +qall
+
+fi
